ee40a1cf170f1caf10be3291172e232cf982d944
Brahian Sanabria <brahiansanabria2005@gmail.com>
2024-10-10 00:01:44 -0500

    first commit

---
+++ src/main/java/co/edu/uco/taskpeak/proyectos/init/MsTpProyectosApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.taskpeak.proyectos.init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTpProyectosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTpProyectosApplication.class, args);
+	}
+
+}
